4613ee3347d1620a3158fc9de11911dacfd82868
MussadiqKhan <musadiq.khan@gmail.com>
2020-10-18 20:48:18 +0500

    Carousel added

--- src/components/deliverables/deliverables.component.jsx
+++ src/components/deliverables/deliverables.component.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./deliverables.styles.css";
+import { Carousel } from "react-bootstrap";
+import image1 from "../../static/images/image1.jpg";
+import image2 from "../../static/images/image2.jpg";
+import image3 from "../../static/images/image3.jpg";
+import image4 from "../../static/images/image4.jpg";
 
 const Deliverables = () => {
   return (
@@ -38,6 +43,23 @@ const Deliverables = () => {
                 allow="vr"
               />
             </div>
+          </div>
+          <div className="carousel">
+            <h1>Immaculate. Impartial. [ESX]</h1>
+            <Carousel>
+              <Carousel.Item>
+                <img className="d-block w-100" src={image2} alt="First slide" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="d-block w-100" src={image1} alt="Third slide" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="d-block w-100" src={image3} alt="Third slide" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="d-block w-100" src={image4} alt="Third slide" />
+              </Carousel.Item>
+            </Carousel>
           </div>
         </div>
       </div>
